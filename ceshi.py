60687eb9372e5c61291a3eba0c5dd7010b26d618
liuruyan <jiaohongbao04@gmail.com>
2024-10-03 20:46:09 +0800

    second test

--- ceshi.py
+++ ceshi.py
@@ -5,6 +5,11 @@ import numpy
 from PIL import Image
 
 
+output_folder='./result/'
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 slide=openslide.open_slide('./test1.svs')
 
 dz_generator=DeepZoomGenerator(slide,tile_size=256,overlap=0)
@@ -14,8 +19,8 @@ for level in range(dz_generator.level_count):
     for col in range(tiles[0]):
         for row in range(tiles[1]):
             tile=dz_generator.get_tile(level,(col,row))
-            #tile_image=Image.fromarray(tile)
-            tile.save(f'output_tile_level_{level}_col_{col}_row_{row}.jpg','JPEG')
+            output=os.path.join(output_folder,f'output_tile_level_{level}_col_{col}_row_{row}.jpg')
+            tile.save(output,'JPEG')
 
 slide.close()
 
